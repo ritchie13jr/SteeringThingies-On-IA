f25af89e924a9aa7025dc7ed7807c0508dd07521
Ricardo Fuster Arcos <165805818+ritchie13jr@users.noreply.github.com>
2026-01-15 14:30:46 +0100

    3 ejercici

--- Assets/Exercises/Exer_Steerings/KeepPosition.cs
+++ Assets/Exercises/Exer_Steerings/KeepPosition.cs
@@ -10,20 +10,34 @@ namespace Steerings
         public float requiredDistance;
         public float requiredAngle;
 
-        /* COMPLETE */ 
+        /* COMPLETE */
+
+        public override GameObject GetTarget()
+        {
+            return target;
+        }
 
         public override Vector3 GetLinearAcceleration()
         {
-            /* COMPLETE */
-            return Vector3.zero; // remove this line when exercise completed
+            return GetLinearAcceleration(Context, target, requiredDistance, requiredAngle);
         }
 
         
         public static Vector3 GetLinearAcceleration (SteeringContext me, GameObject target,
                                                      float distance, float angle)
         {
-            /* COMPLETE */
-            return Vector3.zero; // remove this line when exercise completed
+            float l_TargetOrientation = Utils.VectorToOrientation(target.transform.right);
+
+            float l_DesiredAngle = l_TargetOrientation + angle;
+
+            Vector3 l_DesiredDirectionFromTarget = Utils.OrientationToVector(l_DesiredAngle).normalized;
+
+            Vector3 l_DisplacementFromTarget = l_DesiredDirectionFromTarget * distance;
+            Vector3 l_DesiredPosition = target.transform.position + l_DisplacementFromTarget;
+
+            SURROGATE_TARGET.transform.position = l_DesiredPosition;
+
+            return Arrive.GetLinearAcceleration(me, SURROGATE_TARGET); // remove this line when exercise completed
         }
 
     }
